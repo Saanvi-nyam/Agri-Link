37776738e0c36d508fec3da433a897b9c69946a0
yajathome <119949745+yajathome@users.noreply.github.com>
2024-11-13 12:00:43 +0530

    nl

--- Cropdoc/routes.py
+++ Cropdoc/routes.py
@@ -1,7 +1,15 @@
 from . import app
-from flask import render_template
+import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+import json
+import matplotlib.pyplot as plt
+from flask import render_template, request
 
 @app.route("/", methods=["GET", "POST"])
 def crop_doc():
+    if request.method == "POST":
+        file = request.files["cropdocFile"]
+        print(file.filepath)
     return render_template("cropdoc.html")
 
